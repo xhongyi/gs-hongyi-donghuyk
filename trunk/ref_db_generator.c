756168d60f0ee3aff413489680f82556b71880f3
blepus <blepus@c01c73e4-119b-c190-b91f-0dae92411588>
2011-10-06 01:16:58 +0000

    DHL:change ref_db_generator.c and add getrefseq function

--- trunk/ref_db_generator.c
+++ trunk/ref_db_generator.c
@@ -1,32 +1,57 @@
-/*****/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-int string_size = 128;
+void getRefSeq(char * result_string, int coordinate, int size, int sting_size);
+void RefGenerator(char * gen_file, char * ref_file, int string_size);
 
-int main()
-{
+int main() {
+	int string_size = 30;
+	int coordinate  = 570+50;
+	int size     	= 200;
+ 	char * result_string 	= (char*) malloc(size+1);
+ 	char * ref_file 	= (char*) malloc(sizeof(char)*4);	
+ 	char * gen_file 	= (char*) malloc(sizeof(char)*7);
+
+	gen_file[0] = 'r';
+	gen_file[1] = 'e';
+	gen_file[2] = 'f';
+	gen_file[0] = 'r';
+	gen_file[1] = 'e';
+	gen_file[2] = 's';
+	gen_file[3] = 'u';
+	gen_file[4] = 'l';
+	gen_file[5] = 't';
+	RefGenerator(gen_file, ref_file, string_size);
+	getRefSeq(result_string, coordinate, size, string_size);	
+ 	fprintf (stdout,"search : %s \n", result_string);
+
+ 	fprintf (stdout,"reference file name : %s \n", ref_file);
+ 	fprintf (stdout,"generated file name : %s \n", gen_file);
+ 	free(result_string);
+ 	free(ref_file);
+	free(gen_file);
+ 	return 0;
+}
+
+void RefGenerator(char * gen_file, char * ref_file, int string_size) {
  	FILE * pFileR;
  	FILE * pFileW;
- 	char mystring [string_size];
- 	char newstring [string_size+1];
-//	int size1;
-//	int size2;
+ 	char * mystring  = (char*) malloc(string_size);
+ 	char * newstring = (char*) malloc(string_size+1);
 	int size_ref_read = string_size;
 	int i = 0;
 	int j = 0;
 	int flag_ref_end = 0;
 
- 	pFileR = fopen ("ref" 	, "r");
- 	pFileW = fopen ("result" , "w");
+ 	pFileR = fopen (ref_file, "r");
+ 	pFileW = fopen (gen_file, "w");
 
 	while (flag_ref_end == 0) {
 		if (i == 0) {
 			size_ref_read = fread (mystring, 1, string_size, pFileR);
 			i = size_ref_read;
-// 			fprintf (stdout,"read size %i \n", size_ref_read);
+			//fprintf (stdout,"read size %i \n", size_ref_read);
 		}
 		switch (mystring[string_size-i]){
 			case 'A': newstring[j]='A'; j=j+1; break;
@@ -40,7 +65,7 @@ int main()
 			default: break;
 		}
 		i = i - 1;
-// 			fprintf (stdout,"i: %i ",i);
+ 			//fprintf (stdout,"i: %i j:%i \n",i,j);
 		if (j == string_size) {
 			newstring[j] = '\0';
  			fprintf (stdout,"%s \n", newstring);
@@ -48,8 +73,8 @@ int main()
 			fwrite (newstring, 1, j+1, pFileW);
 			j = 0;
 		} 
-		else if ((size_ref_read < string_size)&&(i==0)) {
-// 			fprintf (stdout,"Here \n");
+		else if ((size_ref_read < string_size)&&(i<=0)) {
+			//fprintf (stdout,"Here \n");
 			newstring[j] = '\0';
  			fprintf (stdout,"%s \n", newstring);
 			newstring[j] = '\n';
@@ -58,29 +83,37 @@ int main()
 			flag_ref_end = 1;
 		}
 	}
-
  	fclose (pFileR);
  	fclose (pFileW);
- 	return 0;
+	free (mystring);
+	free (newstring);
 }
 
-// 			fprintf (stdout,"read size %i \n", size_ref_read);
-// 	fprintf (stdout,"(j:%i, i:%i) ", j, i);
-// 	if (pFile == NULL) perror ("Error opening file");
-// 	else if ( fgets (mystring , 128 , pFile) != NULL ){
-// 		puts (mystring);
-// 	}
-	//size1 = fread (mystring , 1, 128, pFile);
-//	puts (mystring);
-//	size2 = fread (mystring , 1, 128, pFile);
- //	puts (mystring);
- //	fprintf (stdout,"\n\n");
- //	fprintf (stdout,"size1 %i \n", size1);
- //	fprintf (stdout,"size2 %i \n", size2);
-//
-//
- //	fprintf (stdout,"test %1c \n", mystring[0]);
- //	fprintf (stdout,"test %1c \n", mystring[1]);
- //	fprintf (stdout,"test %1c \n", mystring[2]);
- //	fprintf (stdout,"test %1c \n", mystring[3]);
- //////	fprintf (stdout,"test %1c \n", mystring[4]);
+void getRefSeq(char *result_string, int coordinate, int size, int string_size) {
+ 	FILE * pFileS;
+	char * search_string;
+	int  size_opt;
+	int  coordinate_opt;
+	int  boundary;
+	int  read_number;
+	int  i;
+
+	boundary   	= coordinate-(coordinate/string_size)*string_size + size;	// boundary checking
+	size_opt   	= size + boundary / string_size;				// size optimization
+	coordinate_opt 	= coordinate+coordinate/string_size;				// add # of new line characters
+ 	search_string = (char*) malloc(size_opt+1);	
+ 	pFileS = fopen ("result" , "r");
+	fseek (pFileS, coordinate_opt, SEEK_SET );
+	read_number = fread (search_string, 1, size_opt, pFileS);
+	for (i = 0; i<size ; i++) {
+		if (search_string[i] == '\n') {
+			result_string[i] = search_string[i+1];
+		}
+		else {
+			result_string[i] = search_string[i];
+		}
+	}
+	result_string[size] = '\0';	
+	fclose (pFileS);
+	free(search_string);
+}
