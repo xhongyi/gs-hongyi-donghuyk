b7c12dc7160bf7a7f4716f58328e8c6b813a0e01
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-10-06 18:49:26 +0000

    added test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Oct 6, 2011
+ *      Author: mac
+ */
+#include <iostream>
+#include "hash_generator.h"
+
+using namespace std;
+
+int main() {
+	hashTableGenerator("result");
+	return 0;
+}
