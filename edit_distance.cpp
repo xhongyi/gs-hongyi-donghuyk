f1a85ef1378437732af4ec34a0380423b3d98490
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-11-13 16:05:35 +0000

    edit_distance.cpp debuged and passes the test

--- edit_distance.cpp
+++ edit_distance.cpp
@@ -11,11 +11,13 @@
 #define		_MAIN_PATH_		0
 #define		_DELETE_PATH_	1
 #define		_INSERT_PATH_	2
-#define		_UN_FILLED_	1000
+#define		_UN_FILLED_		1000
 
 #include "edit_distance.h"
 #include <algorithm>
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
 /*
  *          reference string
  * 		  a p p l i c a t i o n
@@ -87,7 +89,7 @@ void allocatePath() {
 }
 
 void initializePath() {
-	for (int i = 1; i <= max_indel_num * 2 + 1; i++) {
+	for (int i = 0; i < max_indel_num * 2 + 3; i++) {
 		//For the insertion lanes, the front point is shift right.
 		//
 		path[i].front_idx = (i < main_lane) ? main_lane - i : 0;
@@ -100,7 +102,7 @@ void initializePath() {
 ED_result editDistanceCal(string test_read, string ref_read) {
 	//Return result;
 	ED_result result;
-	result.compare_result = "\0";
+	strcpy(result.compare_result, "\0");
 
 	//Initialize path
 	initializePath();
@@ -120,6 +122,7 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 
 	//Pick a lane to go through
 	while (!ED_finished) {
+		cout << "Here 0" << endl;
 		//First Pick a path.
 		while (path[cur_lane].path_cost[path[cur_lane].front_idx] != cur_dist) {
 			if (cur_lane == max_indel_num * 2 + 1) { //Next is boundary
@@ -127,6 +130,7 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 					//TODO: BREAK AND END.
 					ED_finished = true;
 					result.correct = false;
+					break;
 				} else {
 					//Go to next tier
 					cur_lane = 1;
@@ -140,14 +144,19 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 		//Indicate if the lane should be stopped
 		int slide_stop = 0;
 
+		cout << "cur_dist: " << cur_dist << endl;
+		cout << "cur_lane: " << cur_lane << endl;
+
 		//Slide down the lane.
 		while (!ED_finished) {
+			cout << "lane_front_idx: " << path[cur_lane].front_idx << endl;
 			//Conservative test, speed up common case
 			if (path[cur_lane].front_idx >= READ_LENGTH - max_indel_num) {
 				//Test if it's the last element
-				if ( (cur_lane <= main_lane && path[cur_lane].front_idx == READ_LENGTH) //Insertion lane
-						|| (cur_lane > main_lane && path[cur_lane].front_idx  //Deletion Lane
-								== READ_LENGTH + main_lane - cur_lane) ) {
+				if ((cur_lane <= main_lane && path[cur_lane].front_idx
+						== READ_LENGTH) //Insertion lane
+						|| (cur_lane > main_lane && path[cur_lane].front_idx //Deletion Lane
+								== READ_LENGTH + main_lane - cur_lane)) {
 					ED_finished = true;
 					result.correct = true;
 					break;
@@ -155,23 +164,33 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 			}
 
 			//Compute string idx:
-			test_idx = path[cur_lane].front_idx + cur_lane_num - main_lane_num;
+			test_idx = path[cur_lane].front_idx + cur_lane - main_lane;
 			ref_idx = path[cur_lane].front_idx;
 
-			//slide down
-			if ( !(path[cur_lane].path_cost[path[cur_lane].front_idx + 1] == cur_dist //If can just slide
-					|| test_read[test_idx] == ref_read[ref_idx]) )
+			//test if can slide down
+			if (!(path[cur_lane].path_cost[path[cur_lane].front_idx + 1]
+					== cur_dist //If can just slide
+					|| test_read[test_idx] == ref_read[ref_idx]))
 				slide_stop = 1;
 
 			//Check neighbor lanes and update them. Modify -> increment -> modify
 			//Check lower neighbor
-			if (path[cur_lane + 1].path_cost[path[cur_lane].front_idx] > cur_dist + slide_stop + 1)
-				path[cur_lane + 1].path_cost[path[cur_lane].front_idx] = cur_dist + slide_stop + 1;
+			if (path[cur_lane + 1].path_cost[path[cur_lane].front_idx]
+					> cur_dist + slide_stop + 1)
+				path[cur_lane + 1].path_cost[path[cur_lane].front_idx]
+						= cur_dist + slide_stop + 1;
 			//Increment
 			path[cur_lane].front_idx++;
+			//Check itself
+			if (path[cur_lane].path_cost[path[cur_lane].front_idx] > cur_dist
+					+ slide_stop)
+				path[cur_lane].path_cost[path[cur_lane].front_idx] = cur_dist
+						+ slide_stop;
 			//Check upper neighbor
-			if (path[cur_lane - 1].path_cost[path[cur_lane].front_idx] > cur_dist + slide_stop + 1)
-				path[cur_lane + 1].path_cost[path[cur_lane].front_idx] = cur_dist + slide_stop + 1;
+			if (path[cur_lane - 1].path_cost[path[cur_lane].front_idx]
+					> cur_dist + slide_stop + 1)
+				path[cur_lane - 1].path_cost[path[cur_lane].front_idx]
+						= cur_dist + slide_stop + 1;
 
 			//stop if can't slide anymore
 			if (slide_stop == 1)
@@ -179,32 +198,58 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 		}
 	}
 
+	cout << "Path generated" << endl;
+
 	//Tracing back period
 	if (result.correct == false)
 		return result;
 	else { //If pass the test, trace back
-		int cur_idx = (cur_lane <= main_lane_num) ? READ_LENGTH + 1 : READ_LENGTH + main_lane_num - cur_lane + 1;
-		cur_dist = path[cur_lane][cur_idx];
+		char temp_result [20]; //Temp string. Used for appending.
+		int cur_idx = (cur_lane <= main_lane) ? READ_LENGTH : READ_LENGTH
+				+ main_lane - cur_lane;
+		cur_dist = path[cur_lane].path_cost[cur_idx];
 		int same_count = 0;
-		while (cur_lane != main_lane_num && cur_idx != 0) {
+		cout << "cur_lane: " << cur_lane << " cur_idx: " << cur_idx << endl;
+		while (cur_lane != main_lane || cur_idx != 0) {
+			cout << "cur_lane: " << cur_lane << " cur_idx: " << cur_idx << endl;
 
 			//If we should have an insertion
-			if (cur_idx == 0 || path[cur_lane - 1].path_cost[cur_idx] < path[cur_lane].path_cost[cur_idx - 1]) {
-				if (same_count != 0)
-					sprintf (result.compare_result, "^%c%d%s", test_read[cur_idx + cur_lane - main_lane_num - 1], same_count, result.compare_result);
-				else
-					sprintf (result.compare_result, "^%c%s", test_read[cur_idx + cur_lane - main_lane_num - 1], result.compare_result);
+			if (cur_idx == 0 || path[cur_lane - 1].path_cost[cur_idx]
+					< path[cur_lane].path_cost[cur_idx - 1]) {
+				if (same_count != 0) {
+					cout << "Here m" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "^%c%d%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							same_count, temp_result);
+				}
+				else {
+					cout << "Here n" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "^%c%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							temp_result);
+				}
 				cur_lane--;
 				same_count = 0;
 				continue;
 			}
 
 			//If we should have a deletion
-			if (path[cur_lane + 1].path_cost[cur_idx - 1] < path[cur_lane].path_cost[cur_idx - 1]) {
-				if (same_count != 0)
-					sprintf (result.compare_result, "`%c%d%s", ref_read[cur_idx], same_count, result.compare_result);
-				else
-					sprintf (result.compare_result, "`%c%d%s", ref_read[cur_idx], result.compare_result);
+			if (path[cur_lane + 1].path_cost[cur_idx - 1]
+					< path[cur_lane].path_cost[cur_idx - 1]) {
+				if (same_count != 0) {
+					cout << "Here x" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "`%c%d%s",
+							ref_read[cur_idx - 1], same_count,
+							temp_result);
+				} else {
+					cout << "Here y" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "`%c%s", ref_read[cur_idx - 1],
+							temp_result);
+				}
 				cur_lane++;
 				cur_idx--;
 				same_count = 0;
@@ -212,13 +257,24 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 			}
 
 			//Check if we have a mismatch
-			if (path[cur_lane].path_cost[cur_idx - 1] < path[cur_lane].path_cost[cur_idx]) {
-				if (same_count != 0)
-					sprintf (result.compare_result, "%c%d%s", test_read[cur_idx + cur_lane - main_lane_num - 1],
-							same_count, result.compare_result);
-				else
-					sprintf (result.compare_result, "%c%s", test_read[cur_idx + cur_lane - main_lane_num - 1],
-							result.compare_result);
+			if (path[cur_lane].path_cost[cur_idx - 1]
+					< path[cur_lane].path_cost[cur_idx]) {
+				cout << "Here1" << endl;
+				if (same_count != 0) {
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "%c%d%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							same_count, temp_result);
+
+					cout << "Here2" << endl;
+				} else {
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "%c%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							temp_result);
+					cout << "Here3" << endl;
+				}
+				cur_idx--;
 				same_count = 0;
 				continue;
 			}
@@ -226,9 +282,14 @@ ED_result editDistanceCal(string test_read, string ref_read) {
 			//Move to the next element
 			cur_idx--;
 			same_count++;
+			cout << "same_count: " << same_count << endl;
 		}
-		if (same_count != 0) //If we have some same count at the begining
-			sprintf(result.compare_result, "%d%s", same_count, result.compare_result);
+		if (same_count != 0) { //If we have some same count at the begining
+			cout << "Here t" << endl;
+			strcpy(temp_result, result.compare_result);
+			sprintf(result.compare_result, "%d%s", same_count,
+					temp_result);
+		}
 	}
 	return result;
 }
