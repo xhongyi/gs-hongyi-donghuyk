11590a80b37866c315ed76e6e0ed38215775497f
blepus <blepus@c01c73e4-119b-c190-b91f-0dae92411588>
2011-11-15 03:06:38 +0000

    DHL: add counter for calculating total number of edit-distance calculation

--- test_distribution.c
+++ test_distribution.c
@@ -15,20 +15,24 @@ int main(int argc, char* argv[]) {
 	char * file_ref		= (char*) malloc(sizeof(char)*20);	
 	char * file_hash	= (char*) malloc(sizeof(char)*20);
 	char * file_dist	= (char*) malloc(sizeof(char)*20);
+	if (argc < 2) {
+		fprintf(stdout,"Need parameter [frist/cheap]\n");
+		return 0;
+	}
 	if (!strcmp(argv[1], "first")){
 		set_select_cheapest(false);
 		fprintf(stdout,"Mode : First Select\n");
-		sprintf(file_dist, "%s%i", "toy_dist_first_", 0);
+		sprintf(file_dist, "%s%i", "result_dist_first_", 0);
 	} else if (!strcmp(argv[1], "cheap")){
 		set_select_cheapest(true);
 		fprintf(stdout,"Mode : Cheapest Select\n");
-		sprintf(file_dist, "%s%i", "toy_dist_cheap_", 0);
+		sprintf(file_dist, "%s%i", "result_dist_cheap_", 0);
 	} else {
 		fprintf(stdout,"Error select mode [frist/cheap]\n");
 		return 0;
 	}
-	sprintf(file_ref,  "%s%i", "toy_ref_", 0);
-	sprintf(file_hash, "%s%i", "toy_hash_", 0);
+	sprintf(file_ref,  "%s%i", "result_ref_", 0);
+	sprintf(file_hash, "%s%i", "result_hash_", 0);
 	match_distribution(file_hash, file_ref, file_dist);
 
 // Fee Memory Allocation 
