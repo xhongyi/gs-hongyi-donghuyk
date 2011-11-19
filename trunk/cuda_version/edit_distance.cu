47c758c75611cf6257588a26f47dc5f99ca876dc
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-11-19 03:52:36 +0000

    before switch platform

---
+++ trunk/cuda_version/edit_distance.cu
@@ -0,0 +1,296 @@
+/*
+ * edit_distance.cpp
+ *
+ *  Created on: Oct 29, 2011
+ *      Author: mac
+ */
+
+/*
+ *
+ */
+#define		_MAIN_PATH_		0
+#define		_DELETE_PATH_	1
+#define		_INSERT_PATH_	2
+#define		_UN_FILLED_		1000
+
+#include "edit_distance.h"
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+/*
+ *          reference string
+ * 		  a p p l i c a t i o n
+ *      0 1 2
+ * t  a 1 0 1 2
+ * e  p 2 1 0 1 2    <----delete_path
+ * s  p   2 1 0 1 2
+ * t  p     2 1 1 2 3
+ *    l       2 1 2 3
+ * s  o     ^   2 2 3 <------main_path
+ * t  c     |     3 2
+ * i  a     |         2
+ * n  t     |           2
+ * g  i     |             2
+ *    o     |               2
+ *	        |
+ *	    insert_path
+ *
+ *
+ *          reference string
+ *        a p p l i c a t i o n
+ *      0 1 2 3
+ * t  a 1 0 1 2 3
+ * e  p 2 1 0 1 2 3  <----bondary_path
+ * s  p 3 2 1 0 1 2 3
+ * t  p   3 2 1 1 2 3 4
+ *    l     3 2 1 2 3 4 5
+ * s  o     ^ 3 2 2 3 4 5 <----delete_path
+ * t  c     |   3 3 2 3 4
+ * i  a     |       3 2 3 4
+ * n  i     |         3 3 3 4
+ * g  o     |             4 3 4
+ *    n     |             ^ 4 3 <------main_path
+ *	        |             |
+ *	   bondary_path  insert_path
+ *      |
+ *      | | | 3 3 3 3 4 5
+ *      | | 2 2 2 2 3 4 5
+ *      | 1 1 1 1 2 3 4 4 4 4 4
+ *      0 0 0 0 1 2 3 3 3 3 3 3
+ *      1 1 1 1 1 2 2 2 3 4 4 |
+ *      2 2 2 2 2 3 3 3   | | |
+ *      3 3 3 3 3       | | | |
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+struct ED_path {
+	int path_cost[READ_LENGTH + 1];
+	int front_idx;
+};
+
+ED_path * path;
+
+int main_lane;
+int cur_lane;
+int cur_dist;
+
+void allocatePath() {
+	path = new ED_path[max_indel_num * 2 + 3];
+	main_lane = max_indel_num + 1;
+}
+
+void initializePath() {
+	for (int i = 0; i < max_indel_num * 2 + 3; i++) {
+		//For the insertion lanes, the front point is shift right.
+		//
+		path[i].front_idx = (i < main_lane) ? main_lane - i : 0;
+		for (int j = 0; j <= READ_LENGTH; j++) {
+			path[i].path_cost[j] = _UN_FILLED_;
+		}
+	}
+}
+
+ED_result editDistanceCal(string test_read, string ref_read) {
+	//Return result;
+	ED_result result;
+	strcpy(result.compare_result, "\0");
+
+	//Initialize path
+	initializePath();
+
+	//Current distance pointer set to 0
+	cur_dist = 0;
+	//Start at the main lane
+	cur_lane = main_lane;
+	//Set the first cost of the main lane to 0
+	path[cur_lane].path_cost[0] = 0;
+
+	bool ED_finished = false;
+
+	//String pointers. Used in comparing characters
+	int test_idx;
+	int ref_idx;
+
+	//Pick a lane to go through
+	while (!ED_finished) {
+		//cout << "Here 0" << endl;
+		//First Pick a path.
+		while (path[cur_lane].path_cost[path[cur_lane].front_idx] != cur_dist) {
+			if (cur_lane == max_indel_num * 2 + 1) { //Next is boundary
+				if (cur_dist >= max_diff_num) { //Check if we exceeds the max diff tolarence
+					//TODO: BREAK AND END.
+					ED_finished = true;
+					result.correct = false;
+					break;
+				} else {
+					//Go to next tier
+					cur_lane = 1;
+					cur_dist++;
+				}
+			} else {
+				cur_lane++;
+			}
+		}
+
+		//Indicate if the lane should be stopped
+		int slide_stop = 0;
+
+		//cout << "cur_dist: " << cur_dist << endl;
+		//cout << "cur_lane: " << cur_lane << endl;
+
+		//Slide down the lane.
+		while (!ED_finished) {
+			//cout << "lane_front_idx: " << path[cur_lane].front_idx << endl;
+			//Conservative test, speed up common case
+			if (path[cur_lane].front_idx >= READ_LENGTH - max_indel_num) {
+				//Test if it's the last element
+				if ((cur_lane <= main_lane && path[cur_lane].front_idx
+						== READ_LENGTH) //Insertion lane
+						|| (cur_lane > main_lane && path[cur_lane].front_idx //Deletion Lane
+								== READ_LENGTH + main_lane - cur_lane)) {
+					ED_finished = true;
+					result.correct = true;
+					break;
+				}
+			}
+
+			//Compute string idx:
+			test_idx = path[cur_lane].front_idx + cur_lane - main_lane;
+			ref_idx = path[cur_lane].front_idx;
+
+			//test if can slide down
+			if (!(path[cur_lane].path_cost[path[cur_lane].front_idx + 1]
+					== cur_dist //If can just slide
+					|| test_read[test_idx] == ref_read[ref_idx]))
+				slide_stop = 1;
+
+			//Check neighbor lanes and update them. Modify -> increment -> modify
+			//Check lower neighbor
+			if (path[cur_lane + 1].path_cost[path[cur_lane].front_idx]
+					> cur_dist + slide_stop + 1)
+				path[cur_lane + 1].path_cost[path[cur_lane].front_idx]
+						= cur_dist + slide_stop + 1;
+			//Increment
+			path[cur_lane].front_idx++;
+			//Check itself
+			if (path[cur_lane].path_cost[path[cur_lane].front_idx] > cur_dist
+					+ slide_stop)
+				path[cur_lane].path_cost[path[cur_lane].front_idx] = cur_dist
+						+ slide_stop;
+			//Check upper neighbor
+			if (path[cur_lane - 1].path_cost[path[cur_lane].front_idx]
+					> cur_dist + slide_stop + 1)
+				path[cur_lane - 1].path_cost[path[cur_lane].front_idx]
+						= cur_dist + slide_stop + 1;
+
+			//stop if can't slide anymore
+			if (slide_stop == 1)
+				break;
+		}
+	}
+
+	//cout << "Path generated" << endl;
+
+	//Tracing back period
+	if (result.correct == false)
+		return result;
+	else { //If pass the test, trace back
+		char temp_result [20]; //Temp string. Used for appending.
+		int cur_idx = (cur_lane <= main_lane) ? READ_LENGTH : READ_LENGTH
+				+ main_lane - cur_lane;
+		cur_dist = path[cur_lane].path_cost[cur_idx];
+		int same_count = 0;
+		//cout << "cur_lane: " << cur_lane << " cur_idx: " << cur_idx << endl;
+		while (cur_lane != main_lane || cur_idx != 0) {
+			//cout << "cur_lane: " << cur_lane << " cur_idx: " << cur_idx << endl;
+
+			//If we should have an insertion
+			if (cur_idx == 0 || path[cur_lane - 1].path_cost[cur_idx]
+					< path[cur_lane].path_cost[cur_idx - 1]) {
+				if (same_count != 0) {
+					//cout << "Here m" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "^%c%d%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							same_count, temp_result);
+				}
+				else {
+					//cout << "Here n" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "^%c%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							temp_result);
+				}
+				cur_lane--;
+				same_count = 0;
+				continue;
+			}
+
+			//If we should have a deletion
+			if (path[cur_lane + 1].path_cost[cur_idx - 1]
+					< path[cur_lane].path_cost[cur_idx - 1]) {
+				if (same_count != 0) {
+					//cout << "Here x" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "`%c%d%s",
+							ref_read[cur_idx - 1], same_count,
+							temp_result);
+				} else {
+					//cout << "Here y" << endl;
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "`%c%s", ref_read[cur_idx - 1],
+							temp_result);
+				}
+				cur_lane++;
+				cur_idx--;
+				same_count = 0;
+				continue;
+			}
+
+			//Check if we have a mismatch
+			if (path[cur_lane].path_cost[cur_idx - 1]
+					< path[cur_lane].path_cost[cur_idx]) {
+				//cout << "Here1" << endl;
+				if (same_count != 0) {
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "%c%d%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							same_count, temp_result);
+
+					//cout << "Here2" << endl;
+				} else {
+					strcpy(temp_result, result.compare_result);
+					sprintf(result.compare_result, "%c%s",
+							test_read[cur_idx + cur_lane - main_lane - 1],
+							temp_result);
+					//cout << "Here3" << endl;
+				}
+				cur_idx--;
+				same_count = 0;
+				continue;
+			}
+
+			//Move to the next element
+			cur_idx--;
+			same_count++;
+			//cout << "same_count: " << same_count << endl;
+		}
+		if (same_count != 0) { //If we have some same count at the begining
+			//cout << "Here t" << endl;
+			strcpy(temp_result, result.compare_result);
+			sprintf(result.compare_result, "%d%s", same_count,
+					temp_result);
+		}
+	}
+	return result;
+}
+
---
+++ trunk/cuda_version/fragment_match.cu
@@ -0,0 +1,177 @@
+/*
+ * fragment_match.cpp
+ *
+ *  Created on: Oct 8, 2011
+ *      Author: mac
+ */
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <assert.h>
+#include "common.h"
+#include "fragment_match.h"
+
+int* hash_table;
+int* coordinate;
+int* prefilter;
+
+void loadHash(string hash_name) {
+	char * temp = new char[hash_name.size() + 1];
+	hash_name.copy(temp, hash_name.size(), 0);
+	hashReconstructorChar(&hash_table, &coordinate, &prefilter, temp);
+	delete[] temp;
+}
+
+bool searchKey(int* start_coor, int target_coor, int entry_coor, int entry_size) {
+	if (entry_size == 0)
+		return false;
+	int lower_bound = entry_coor + 1;
+	int upper_bound = entry_coor + entry_size;
+	int mid = lower_bound + entry_size / 2;
+	while (lower_bound < upper_bound) {
+		if (coordinate[mid] == target_coor)
+			break;
+		else if (coordinate[mid] < target_coor)
+			lower_bound = mid + 1;
+		else
+			upper_bound = mid - 1;
+		mid = lower_bound + (upper_bound - lower_bound) / 2;
+	}
+
+	if (coordinate[mid] <= target_coor + max_indel_num && coordinate[mid]
+			>= target_coor - max_indel_num) {
+		*start_coor = coordinate[mid];
+		return true;
+	} else  
+		return false;
+}
+
+bool searchPrevious(int coor_value, int start_key_entry,
+		list<match_result> previous_result) {
+	if (previous_result.size() == 0) {
+		return false;
+	}
+	for (list<match_result>::iterator it_coor = previous_result.begin(); it_coor
+			!= previous_result.end(); ++it_coor) {
+		if (((*it_coor).coordinate == coor_value - start_key_entry * KEY_LENGTH)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool sortPrefilter(key_struct* sort_result, key_struct* sort_input) {
+	if (select_cheapest == false) {
+		for (int i = 0; i < max_diff_num + 1; i++) {
+			sort_result[i].key_number = sort_input[i].key_number;
+			sort_result[i].key_entry = sort_input[i].key_entry;
+			sort_result[i].key_entry_size = sort_input[i].key_entry_size;
+		}
+		return true;
+	}
+	for (int i = 0; i < KEY_NUMBER; i++) {
+		for (int j = 0; j < KEY_NUMBER; j++) {
+			if (sort_input[i].key_entry_size > sort_input[j].key_entry_size) {
+				sort_input[i].order = sort_input[i].order + 1;
+			}
+		}
+	}
+	int loop_index = 0;
+	for (int i = 0; i < max_diff_num + 1; i++) {
+		for (int j = 0; j < KEY_NUMBER; j++) {
+			if (sort_input[j].order == i) {
+				sort_result[loop_index].key_entry = sort_input[j].key_entry;
+				sort_result[loop_index].key_number = sort_input[j].key_number;
+				sort_result[loop_index].key_entry_size
+						= sort_input[j].key_entry_size;
+				loop_index = loop_index + 1;
+			}
+			if (loop_index == max_diff_num + 1)
+				return true;
+		}
+	}
+	return false;
+}
+
+list<match_result> searchFragment(string fragment) {
+	list<match_result> result;
+	key_struct sort_input[KEY_NUMBER];
+	for (int i = 0; i < KEY_NUMBER; i++) {
+		string key = fragment.substr(KEY_LENGTH * i, KEY_LENGTH);
+		int key_hash = hashVal(key);
+		int key_entry = hash_table[key_hash];
+		int key_entry_size = coordinate[key_entry];
+		sort_input[i].order = 0;
+		sort_input[i].key_number = i;
+		sort_input[i].key_entry = key_entry;
+		sort_input[i].key_entry_size = key_entry_size;
+	}
+
+	key_struct keys_input[max_diff_num + 1];
+	sortPrefilter(keys_input, sort_input);
+
+	for (int k = 0; k < max_diff_num + 1; k++) {
+		for (int i = keys_input[k].key_entry + 1; i <= keys_input[k].key_entry + keys_input[k].key_entry_size; i++) {
+			int coor_value = coordinate[i];
+			int diff_num = 0;
+			int tmp_start_coor = 0;
+			int start_coor = 0;
+			bool first_matched = false;
+			if (!searchPrevious(coor_value, keys_input[k].key_number, result)){
+				for (int j = 0; j < KEY_NUMBER; j++) {
+					string segment_str = fragment.substr(j * KEY_LENGTH, KEY_LENGTH);
+					if(!searchKey(&tmp_start_coor, coor_value + (j-keys_input[k].key_number) * KEY_LENGTH, sort_input[j].key_entry, sort_input[j].key_entry_size)) 
+						diff_num++;
+					else if (first_matched == false) {
+						start_coor = tmp_start_coor - j*KEY_LENGTH;
+						first_matched = true;
+					}
+					else if (diff_num > max_diff_num)
+						break;
+				}
+				if (diff_num <= max_diff_num) {
+					match_result temp;
+					temp.coordinate = start_coor;
+					temp.relevance  = diff_num;
+					temp.key_number = keys_input[k].key_number;
+					result.push_back(temp);
+				}       
+			}
+		}
+	}
+	return result;
+}
+
+int binary_filtering_cost(string fragment) {
+	int result = 0;
+	int status = 0;
+	key_struct sort_input[KEY_NUMBER];
+	for (int i = 0; i < KEY_NUMBER; i++) {
+		string key = fragment.substr(KEY_LENGTH * i, KEY_LENGTH);
+		int key_hash = hashVal(key);
+		int key_entry = hash_table[key_hash];
+		int key_entry_size = coordinate[key_entry];
+		sort_input[i].order = 0;
+		sort_input[i].key_number = i;
+		sort_input[i].key_entry = key_entry;
+		sort_input[i].key_entry_size = key_entry_size;
+	}
+
+	key_struct keys_input[max_diff_num + 1];
+	sortPrefilter(keys_input, sort_input);
+
+	if (select_cheapest == true)
+		sortPrefilter(keys_input, sort_input);
+	else {
+		for (int i = 0; i < max_diff_num + 1; i++) {
+			keys_input[i].key_number = sort_input[i].key_number;
+			keys_input[i].key_entry = sort_input[i].key_entry;
+			keys_input[i].key_entry_size = sort_input[i].key_entry_size;
+		}
+	}
+
+	for (int k = 0; k < max_diff_num + 1; k++) {
+		result += keys_input[k].key_entry_size;
+	}
+	return result;
+}
