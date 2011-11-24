30943a9d3308f04a06fdae03e195f46cc3fb1861
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-11-24 23:07:11 +0000

    changed Makefile

---
+++ load_shuffled_read.cpp
@@ -0,0 +1,80 @@
+/*
+ * load_shuffled_read.cpp
+ *
+ *  Created on: Nov 24, 2011
+ *      Author: mac
+ */
+
+#include "load_shuffled_read.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <deque>
+
+void loadTestRead(string test_file_name, string test_fragment[], int& size) {
+	srand(time(NULL) );
+
+	ifstream input_file;
+	input_file.open(test_file_name.c_str());
+	size = 0;
+	char test_char;
+	string temp(FRAGMENT_LENGTH, 'A');
+	//fragment set before shuffle.
+	deque<string> unshuffled_set;
+
+	//Read out the first fragment
+	do {
+		input_file >> test_char;
+	}
+	while (test_char == 'N');
+
+	temp[0] = test_char;
+	for (int i = 1; i < FRAGMENT_LENGTH; i++) {
+		input_file >> test_char;
+		if (test_char == 'N') {
+			i = 0;
+			input_file >> test_char;
+		} else
+			temp[i] = test_char;
+	}
+	size = 1;
+	unshuffled_set.push_back(temp);
+
+	do {
+		if (size == READ_SIZE)
+			break;
+		input_file >> test_char;
+		if (test_char == 'N') {
+			for (int i = 0; i < FRAGMENT_LENGTH; i++) {
+				input_file >> test_char;
+				if (test_char == 'N') {
+					i = 0;
+					input_file >> test_char;
+				}
+				if (test_char != 'N') {
+					temp[i] = test_char;
+				}
+				if (!input_file.good())
+					break;
+			}
+		} else {
+			temp = temp.substr(1, FRAGMENT_LENGTH - 1) + test_char;
+		}
+		size++;
+		unshuffled_set.push_back(temp);
+	}
+	while (input_file.good());
+
+	deque<string>::iterator unshuffled_iter;
+	for (int i = 0; i < size; i++) {
+		unshuffled_iter = unshuffled_set.begin();
+		int increment = (int)(unshuffled_set.size() * ( (rand() % size) / (double) size) );
+		unshuffled_iter = unshuffled_iter + increment;
+		test_fragment[i] = *unshuffled_iter;
+		unshuffled_set.erase(unshuffled_iter);
+	}
+	if (unshuffled_set.empty() )
+		cout << "empty correct!" << endl;
+	else
+		cout << "Not empty!! Incorrect!" << endl;
+}
---
+++ test_shuffle.cpp
@@ -0,0 +1,24 @@
+/*
+ * test_shuffle.cpp
+ *
+ *  Created on: Nov 24, 2011
+ *      Author: mac
+ */
+
+#include "load_shuffled_read.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string test_file_name = "toy_ref_0";
+	string test_input[READ_SIZE];
+	int size;
+	loadTestRead(test_file_name, test_input, size);
+	//cout << "size: " << size << endl;
+	for (int i = 0; i < size; i++) {
+		//cout << "i: " << i << endl;
+		cout << test_input[i] << endl;
+	}
+	return 0;
+}
