1bacb8716d9a80c2607d0929485163e59760352f
blepus <blepus@c01c73e4-119b-c190-b91f-0dae92411588>
2011-11-25 23:42:26 +0000

    DHL: divide output_file

--- trunk/edit_distribution.cpp
+++ trunk/edit_distribution.cpp
@@ -34,28 +34,33 @@ void edit_distribution(string hash_file_name, string ref_file_name,
 	ofstream store_file;
 
 	final_result filter_result;
-	map<int, int> distribution;
-	map<int, int> correct_count;
 	string testee(FRAGMENT_LENGTH, 'A');
 	int monitor_counter = 0; 	// for operation monitoring
 	int correct_counter = 0; 	// for operation monitoring
 	long long monitor_counter2 = 0; // for operation monitoring
 	int gen_coord = 0;
 	char test_char;
-	double accumulate_time = 0;
+	long long accumulate_time = 0;
 
 	// get fragment from reference file
-	store_file.open(output_file_name.c_str());
 
-	//for (int j = 17 ; j < 18 ; j++) {
-	//for (int j = 16 ; j < 17 ; j++) {
+	//for (int j = 0 ; j < 2 ; j++) {
 	for (int j = 0 ; j < MAX_CONTIG_FILE ; j++) {
+		map<int, int> binary_search;
+		map<int, int> distribution;
+		map<int, int> correct_count;
 		char * file_ref	= (char*) malloc(sizeof(char)*50);
 		char * file_hash= (char*) malloc(sizeof(char)*50);
-       		sprintf(file_ref , "%s%i", (char*) ref_file_name.c_str(), j);
-       		sprintf(file_hash, "%s%i", (char*) hash_file_name.c_str(), j);
-		cout << "ref_file_name :" << file_ref  << endl;
-		cout << "hash_file_name:" << file_hash << endl;
+		char * file_store= (char*) malloc(sizeof(char)*50);
+       		sprintf(file_ref ,  "%s%i", (char*) ref_file_name.c_str(), j);
+       		sprintf(file_hash,  "%s%i", (char*) hash_file_name.c_str(), j);
+       		sprintf(file_store, "%s%i", (char*) output_file_name.c_str(), j);
+		cout << "ref_file_name  :" << file_ref  << endl;
+		cout << "hash_file_name :" << file_hash << endl;
+		cout << "store_file_name:" << file_store << endl;
+
+		// store file 
+		store_file.open(file_store);
 
 		// reference file load at string
 		string ref;
@@ -77,6 +82,8 @@ void edit_distribution(string hash_file_name, string ref_file_name,
 		input_file >> testee;
 		do {
 			filter_result = searchFragment(testee, &ref);
+
+			binary_search[filter_result.total_binary_search]++;
 			distribution[filter_result.total_edit_perform]++;
 			correct_count[filter_result.total_correct_num]++;
 	
@@ -94,29 +101,60 @@ void edit_distribution(string hash_file_name, string ref_file_name,
 		
 		time_t end_time;
 		time(&end_time);
-		cout << "Start_time: " << ctime(&start_time) << endl;;
-		cout << "End_time  : " << ctime(&end_time) << endl;
-		cout << "TIme Diff : " << difftime(end_time,start_time) << endl;
+		cout << endl;
 		accumulate_time = accumulate_time + difftime(end_time, start_time);
 		ref_file.close();
 		input_file.close();
+
+		long long total_fragment_num0 = 0;
+		long long total_binary_num = 0;
+		store_file << "Number of Binary Search calculation" << endl;
+		for (map<int, int>::iterator p = binary_search.begin(); p != binary_search.end(); p++) {
+			store_file << "index :" << p->first << "	num :" << p->second << endl;
+			total_fragment_num0 = total_fragment_num0 + p->second;
+			total_binary_num = total_binary_num + p->first * p->second;
+		}
+	
+		long long total_fragment_num1 = 0;
+		long long total_edit_num = 0;
+		store_file << endl << "Number of Edit distance calculation" << endl;
+		for (map<int, int>::iterator p = distribution.begin(); p != distribution.end(); p++) {
+			store_file << "index :" << p->first << "	num :" << p->second << endl;
+			total_fragment_num1 = total_fragment_num1 + p->second;
+			total_edit_num = total_edit_num + p->first * p->second;
+		}
+	
+		long long total_fragment_num2 = 0;
+		long long total_pass_num = 0;
+		store_file << endl << "Number of Edit-distance Passed" << endl;
+		for (map<int, int>::iterator p = correct_count.begin(); p != correct_count.end(); p++) {
+			store_file << "index :" << p->first << "	num :" << p->second << endl;
+			total_fragment_num2 = total_fragment_num2 + p->second;
+			total_pass_num = total_pass_num + p->first * p->second;
+		}
+		store_file << endl;
+		store_file << "---------------------------------------------" << endl;
+		store_file << "total_binary_num   : " << total_binary_num << endl;
+		store_file << "total_fragment_num : " << total_fragment_num1 << endl;
+		store_file << "total_edit_num     : " << total_edit_num << endl;
+		store_file << "total_pass_num     : " << total_pass_num << endl;
+		store_file << "Start_time         : " << ctime(&start_time);
+		store_file << "End_time           : " << ctime(&end_time);
+		store_file << "TIme Diff          : " << difftime(end_time,start_time) << endl;
+		store_file << "Accumulated Time   : " << accumulate_time << endl;
+		store_file << "---------------------------------------------" << endl;
+
+		cout << "---------------------------------------------" << endl;
+		cout << "total_binary_num   : " << total_binary_num << endl;
+		cout << "total_fragment_num : " << total_fragment_num1 << endl;
+		cout << "total_edit_num     : " << total_edit_num << endl;
+		cout << "total_pass_num     : " << total_pass_num << endl;
+		cout << "Start_time         : " << ctime(&start_time);
+		cout << "End_time           : " << ctime(&end_time);
+		cout << "TIme Diff          : " << difftime(end_time,start_time) << endl;
+		cout << "Accumulated Time   : " << accumulate_time << endl;
+		cout << "---------------------------------------------" << endl;
+		store_file.close();
 	}
-	long long total_fragment_num = 0;
-	long long total_perform_num = 0;
-	store_file << "Binary Search Result" << endl;
-	for (map<int, int>::iterator p = distribution.begin(); p
-			!= distribution.end(); p++) {
-		store_file << "index :" << p->first << "	# :" << p->second << endl;
-		total_fragment_num = total_fragment_num + p->second;
-		total_perform_num = total_perform_num + p->first * p->second;
-	}
-	store_file << "total_fragment_num :" << total_fragment_num << endl;
-	store_file << "total_perform_num  :" << total_perform_num << endl;
-	store_file << endl << "Edit-distance Search Result" << endl;
-	for (map<int, int>::iterator p = correct_count.begin(); p
-			!= correct_count.end(); p++) {
-		store_file << "index :" << p->first << "	# :" << p->second << endl;
-	}
-	cout << "Total Time : " << accumulate_time << endl;
-	store_file.close();
+	cout << "Accumulated Time : " << accumulate_time << endl;
 }
