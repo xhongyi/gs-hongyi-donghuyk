099860d957f61f60a542a31a40e2d88a6e2ea2be
blepus <blepus@c01c73e4-119b-c190-b91f-0dae92411588>
2011-12-03 23:41:08 +0000

    DHL load hash table update

--- cuda_version/fragment_match.cu
+++ cuda_version/fragment_match.cu
@@ -15,21 +15,23 @@
 int* hash_table;
 int* coordinate;
 int* dev_coordinate;
-
-void getHashTablePtr(int ** ptr) {
-	*ptr = hash_table;
-}
-
-void getCoordinatePtr(int ** ptr) {
-	*ptr = coordinate;
-}
+int* dev_hash_table;
+texture<int> tex_hash_table;
+texture<int> tex_coordinate;
 
 void loadHash(string hash_name) {
-	long long coordinate_total_num = hashReconstructor(&hash_table, &coordinate, hash_name.c_str());
-	HANDLE_ERROR(cudaMalloc((void**)&dev_coordinate, coordinate_total_num*sizeof(int)));
-	HANDLE_ERROR(cudaMemcpy(dev_coordinate, coordinate, coordinate_total_num*sizeof(int), cudaMemcpyHostToDevice ));
+	long long coord_num = hashReconstructor(&hash_table, &coordinate, hash_name.c_str());
+	HANDLE_ERROR(cudaMalloc((void**)&dev_hash_table, INDEX_NUM*sizeof(int)));
+	HANDLE_ERROR(cudaMalloc((void**)&dev_coordinate, coord_num*sizeof(int)));
+	HANDLE_ERROR(cudaBindTexture(NULL, tex_hash_table, dev_hash_table, INDEX_NUM*sizeof(int)));
+	HANDLE_ERROR(cudaBindTexture(NULL, tex_coordinate, dev_coordinate, coord_num*sizeof(int)));
+	HANDLE_ERROR(cudaMemcpy(dev_coordinate, coordinate, coord_num*sizeof(int), cudaMemcpyHostToDevice ));
+	HANDLE_ERROR(cudaMemcpy(dev_hash_table, hash_table, INDEX_NUM*sizeof(int), cudaMemcpyHostToDevice ));
 }
 
 void freeHash(void) {
+	HANDLE_ERROR( cudaUnbindTexture ( tex_hash_table) );
+	HANDLE_ERROR( cudaUnbindTexture ( tex_coordinate) );
+	HANDLE_ERROR( cudaFree ( dev_hash_table) );
 	HANDLE_ERROR( cudaFree ( dev_coordinate) );
 }
