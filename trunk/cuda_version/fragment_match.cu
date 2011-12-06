c94df3c51803804b5901fe72c8892001ee6f87c1
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-12-06 19:43:59 +0000

    added sorting keys

--- trunk/cuda_version/fragment_match.cu
+++ trunk/cuda_version/fragment_match.cu
@@ -82,9 +82,9 @@ bool sortPrefilter(key_struct* sort_result, key_struct* sort_input) {
 	return true;//false;
 }
 
-__global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* ref,
-		int* hash_table, int* coordinate, int max_diff_num, int max_indel_num,
-		final_result* result) {
+__global__ void searchFragment(GPU_fragment* fragment, int fragment_size,
+		char* ref, int* hash_table, int* coordinate, int max_diff_num,
+		int max_indel_num, final_result* result) {
 	printf("HELLO!!\n");
 	printf("cuda fragment: %c\n", (*fragment).fragment[0]);
 	printf("blockIdx.x: %i, threadIdx.x: %i\n", blockIdx.x, threadIdx.x);
@@ -107,7 +107,6 @@ __global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* 
 		int coor_count = threadIdx.x;
 		int cur_key = 0;
 
-
 		int target_coor =
 				fragment[fragment_count].sorted_keys[cur_key].key_entry + 1
 						+ coor_count
@@ -124,22 +123,28 @@ __global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* 
 			for (int i = 0; i < KEY_NUMBER; i++) { //for each segment
 				if (i - diff_num > KEY_NUMBER - max_diff_num)
 					break;
-				
-				printf("target_coor + (fragment[fragment_count].sorted_keys[i].key_number - fragment[fragment_count].sorted_keys[cur_key].key_number) * KEY_LENGTH: %i\n",
+
+				printf(
+						"target_coor + (fragment[fragment_count].sorted_keys[i].key_number - fragment[fragment_count].sorted_keys[cur_key].key_number) * KEY_LENGTH: %i\n",
 						(target_coor
-                                + (fragment[fragment_count].sorted_keys[i].key_number
-                                        - fragment[fragment_count].sorted_keys[cur_key].key_number)
-                                        * KEY_LENGTH ));
-				printf("fragment[fragment_count].sorted_keys[i].key_entry: %i\n", fragment[fragment_count].sorted_keys[i].key_entry);
-				printf("fragment[fragment_count].sorted_keys[i].key_entry_size: %i\n", fragment[fragment_count].sorted_keys[i].key_entry_size);
-				
+								+ (fragment[fragment_count].sorted_keys[i].key_number
+										- fragment[fragment_count].sorted_keys[cur_key].key_number)
+										* KEY_LENGTH));
+				printf(
+						"fragment[fragment_count].sorted_keys[i].key_entry: %i\n",
+						fragment[fragment_count].sorted_keys[i].key_entry);
+				printf(
+						"fragment[fragment_count].sorted_keys[i].key_entry_size: %i\n",
+						fragment[fragment_count].sorted_keys[i].key_entry_size);
+
 				if (!searchKey(
 						target_coor
 								+ (fragment[fragment_count].sorted_keys[i].key_number
 										- fragment[fragment_count].sorted_keys[cur_key].key_number)
 										* KEY_LENGTH,
 						fragment[fragment_count].sorted_keys[i].key_entry,
-						fragment[fragment_count].sorted_keys[i].key_entry_size, coordinate, max_indel_num)) {
+						fragment[fragment_count].sorted_keys[i].key_entry_size,
+						coordinate, max_indel_num)) {
 					diff_num++;
 					printf("4\n");
 					if (diff_num > max_diff_num)
@@ -162,7 +167,8 @@ __global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* 
 										fragment[fragment_count].fragment,
 										ref_str,
 										fragment[fragment_count].sorted_keys[cur_key].key_number,
-										path, main_lane, max_indel_num, max_diff_num);
+										path, main_lane, max_indel_num,
+										max_diff_num);
 
 				if (edit_result.correct) {
 					printf("find something!\n");
@@ -195,15 +201,18 @@ __global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* 
 
 		__syncthreads();
 
-		printf("print Ids again!! blockIdx.x: %i, threadIdx.x: %i\n", blockIdx.x, threadIdx.x);
+		printf("print Ids again!! blockIdx.x: %i, threadIdx.x: %i\n",
+				blockIdx.x, threadIdx.x);
 
 		if (threadIdx.x == 0) {
 			printf("I'm Here!!!\n");
 			for (int i = 0; i < READ_LENGTH; i++) {
-				result[fragment_count].fragment[i] = fragment[fragment_count].fragment[i];
-				printf("result[%i].fragment[%i]: %c\n", fragment_count, i, result[fragment_count].fragment[i]);
+				result[fragment_count].fragment[i]
+						= fragment[fragment_count].fragment[i];
+				printf("result[%i].fragment[%i]: %c\n", fragment_count, i,
+						result[fragment_count].fragment[i]);
 			}
-			if (size > MAX_COOR_RESULT_NUM )
+			if (size > MAX_COOR_RESULT_NUM)
 				result[fragment_count].spilled = true;
 			else {
 				result[fragment_count].spilled = false;
@@ -216,90 +225,90 @@ __global__ void searchFragment(GPU_fragment* fragment, int fragment_size, char* 
 }
 
 /*
- final_result searchFragment(char* fragment, char* ref, int* hash_table,
- int* coordinate) {
- key_struct sort_input[KEY_NUMBER];
- for (int i = 0; i < KEY_NUMBER; i++) {
- char key[KEY_LENGTH];
- for (int j = 0; j < KEY_NUMBER; j++) {
- key[j] = fragment[j + KEY_LENGTH * i];
- //key = fragment.substr(KEY_LENGTH * i, KEY_LENGTH);
- }
- int key_hash = hashVal(key);
- int key_entry = hash_table[key_hash];
- int key_entry_size = coordinate[key_entry];
- sort_input[i].order = 0;
- sort_input[i].key_number = i;
- sort_input[i].key_entry = key_entry;
- sort_input[i].key_entry_size = key_entry_size;
- }
+final_result searchFragment(char* fragment, char* ref, int* hash_table,
+		int* coordinate) {
+	key_struct sort_input[KEY_NUMBER];
+	for (int i = 0; i < KEY_NUMBER; i++) {
+		char key[KEY_LENGTH];
+		for (int j = 0; j < KEY_NUMBER; j++) {
+			key[j] = fragment[j + KEY_LENGTH * i];
+			//key = fragment.substr(KEY_LENGTH * i, KEY_LENGTH);
+		}
+		int key_hash = hashVal(key);
+		int key_entry = hash_table[key_hash];
+		int key_entry_size = coordinate[key_entry];
+		sort_input[i].order = 0;
+		sort_input[i].key_number = i;
+		sort_input[i].key_entry = key_entry;
+		sort_input[i].key_entry_size = key_entry_size;
+	}
 
- key_struct keys_input[KEY_NUMBER];
- sortPrefilter(keys_input, sort_input);
- previous_result.size = 0;
- final_result return_result;
- return_result.total_binary_search = 0;
- return_result.total_edit_perform = 0;
- return_result.total_correct_num = 0;
+	key_struct keys_input[KEY_NUMBER];
+	sortPrefilter(keys_input, sort_input);
+	previous_result.size = 0;
+	final_result return_result;
+	return_result.total_binary_search = 0;
+	return_result.total_edit_perform = 0;
+	return_result.total_correct_num = 0;
 
- for (int k = 0; k < max_diff_num + 1; k++) { //Getting the seed
- for (int i = keys_input[k].key_entry + 1; i <= keys_input[k].key_entry
- + keys_input[k].key_entry_size; i++) { //Going through each entry in the seed coordinate list
- int coor_value = coordinate[i];
- int diff_num = 0;
- if (!searchPrevious(coor_value, keys_input[k].key_number,
- previous_result)) { //Pre-filtering
- return_result.total_binary_search++;
- for (int j = 0; j < KEY_NUMBER; j++) { //for each segment
- if (j - diff_num > KEY_NUMBER - max_diff_num)
- break;
- if (!searchKey(
- coor_value + (keys_input[j].key_number
- - keys_input[k].key_number) * KEY_LENGTH,
- keys_input[j].key_entry,
- keys_input[j].key_entry_size)) {
- diff_num++;
- if (diff_num > max_diff_num)
- break;
- }
- }
- if (diff_num <= max_diff_num) {
- if (previous_result.size <= PREFILTER_SIZE) {
- previous_result.coor[previous_result.size] = coor_value
- - keys_input[k].key_number * KEY_LENGTH; //start_coor;
- previous_result.size++;
- }
- return_result.total_edit_perform++;
- string ref_str(FRAGMENT_LENGTH, 'A');
- ref_str = (*ref).substr(
- coor_value - keys_input[k].key_number * KEY_LENGTH,
- FRAGMENT_LENGTH); //start_coor;
+	for (int k = 0; k < max_diff_num + 1; k++) { //Getting the seed
+		for (int i = keys_input[k].key_entry + 1; i <= keys_input[k].key_entry
+				+ keys_input[k].key_entry_size; i++) { //Going through each entry in the seed coordinate list
+			int coor_value = coordinate[i];
+			int diff_num = 0;
+			if (!searchPrevious(coor_value, keys_input[k].key_number,
+					previous_result)) { //Pre-filtering
+				return_result.total_binary_search++;
+				for (int j = 0; j < KEY_NUMBER; j++) { //for each segment
+					if (j - diff_num > KEY_NUMBER - max_diff_num)
+						break;
+					if (!searchKey(
+							coor_value + (keys_input[j].key_number
+									- keys_input[k].key_number) * KEY_LENGTH,
+							keys_input[j].key_entry,
+							keys_input[j].key_entry_size)) {
+						diff_num++;
+						if (diff_num > max_diff_num)
+							break;
+					}
+				}
+				if (diff_num <= max_diff_num) {
+					if (previous_result.size <= PREFILTER_SIZE) {
+						previous_result.coor[previous_result.size] = coor_value
+								- keys_input[k].key_number * KEY_LENGTH; //start_coor;
+						previous_result.size++;
+					}
+					return_result.total_edit_perform++;
+					string ref_str(FRAGMENT_LENGTH, 'A');
+					ref_str = (*ref).substr(
+							coor_value - keys_input[k].key_number * KEY_LENGTH,
+							FRAGMENT_LENGTH); //start_coor;
 
- /////////////////////Just For Testing
- char test_char[READ_LENGTH];
- char ref_char[READ_LENGTH];
- strcpy(test_char, fragment.c_str());
- strcpy(ref_char, ref_str.c_str());
- cout << "ref__read: " << ref_char << endl;
- cout << "test_read: " << test_char << endl;
- cout << "key_num__: " << keys_input[k].key_number << endl;
- /////////////////////Testing END
+					/////////////////////Just For Testing
+					char test_char[READ_LENGTH];
+					char ref_char[READ_LENGTH];
+					strcpy(test_char, fragment.c_str());
+					strcpy(ref_char, ref_str.c_str());
+					cout << "ref__read: " << ref_char << endl;
+					cout << "test_read: " << test_char << endl;
+					cout << "key_num__: " << keys_input[k].key_number << endl;
+					/////////////////////Testing END
 
- ED_result edit_result = editDistanceCal(test_char,
- ref_char, keys_input[k].key_number);
- if (edit_result.correct) {
- return_result.total_correct_num++;
- //cout << "ref_read      : " << ref_str << "  coordinate: "<< (*it_result).coordinate << "  Key_number: "<< (*it_result).key_number;
- //cout << "  result: correct " << endl;
- } else {
- //cout << "ref_read      : " << ref_str << "  coordinate: "<< (*it_result).coordinate << "  Key_number: "<< (*it_result).key_number;
- //cout << "  result: not correct" <<endl;
- }
- }
- }
- }
- }
- return return_result;
- }
- */
+					ED_result edit_result = editDistanceCal(test_char,
+							ref_char, keys_input[k].key_number);
+					if (edit_result.correct) {
+						return_result.total_correct_num++;
+						//cout << "ref_read      : " << ref_str << "  coordinate: "<< (*it_result).coordinate << "  Key_number: "<< (*it_result).key_number;
+						//cout << "  result: correct " << endl;
+					} else {
+						//cout << "ref_read      : " << ref_str << "  coordinate: "<< (*it_result).coordinate << "  Key_number: "<< (*it_result).key_number;
+						//cout << "  result: not correct" <<endl;
+					}
+				}
+			}
+		}
+	}
+	return return_result;
+}
 
+*/
--- trunk/cuda_version/test_cuda.cu
+++ trunk/cuda_version/test_cuda.cu
@@ -12,31 +12,56 @@ using namespace std;
 int test_cuda(void) {
 	long long coord_num = loadHash("toy_hash_0");
 	GPU_fragment test_fragment;
-	strcpy(test_fragment.fragment, "GGGTGGTAGGTGCAGAGACGGGAGGGGCAGAGCCGCAGGCACAGCCAAGAGGGCTGAAGAAATGGTAGAACGGAGCAGCTGGTGATGTGTGGGCCCACCGGCCCCAGGCTCCTGTCTCCC");
+	strcpy(
+			test_fragment.fragment,
+			"GGGTGGTAGGTGCAGAGACGGGAGGGGCAGAGCCGCAGGCACAGCCAAGAGGGCTGAAGAAATGGTAGAACGGAGCAGCTGGTGATGTGTGGGCCCACCGGCCCCAGGCTCCTGTCTCCC");
+	//Getting the sort key.
+	key_struct sort_input[KEY_NUMBER];
+	for (int i = 0; i < KEY_NUMBER; i++) {
+		char key[KEY_LENGTH];
+		for (int j = 0; j < KEY_NUMBER; j++) {
+			key[j] = test_fragment.fragment[j + KEY_LENGTH * i];
+			//key = fragment.substr(KEY_LENGTH * i, KEY_LENGTH);
+		}
+		int key_hash = hashVal(key);
+		int key_entry = hash_table[key_hash];
+		int key_entry_size = coordinate[key_entry];
+		sort_input[i].order = 0;
+		sort_input[i].key_number = i;
+		sort_input[i].key_entry = key_entry;
+		sort_input[i].key_entry_size = key_entry_size;
+	}
+
+	sortPrefilter(keys_input, sort_input);
+
 	cout << "Search Fragment: " << test_fragment.fragment << endl;
 
 	GPU_fragment* dev_fragment;
 	final_result* dev_result;
-	final_result* test_result = (final_result*)malloc(sizeof(GPU_fragment));
+	final_result* test_result = (final_result*) malloc(sizeof(GPU_fragment));
 
-	cudaMalloc((void**)&dev_fragment, sizeof(GPU_fragment));
-	cudaMalloc((void**)&dev_result,   sizeof(final_result));
-	cudaMemcpy(dev_fragment, &test_fragment, sizeof(GPU_fragment), cudaMemcpyHostToDevice );
+	cudaMalloc((void**) &dev_fragment, sizeof(GPU_fragment));
+	cudaMalloc((void**) &dev_result, sizeof(final_result));
+	cudaMemcpy(dev_fragment, &test_fragment, sizeof(GPU_fragment),
+			cudaMemcpyHostToDevice);
 
 	searchFragment <<<1, 1>>> (dev_fragment, 1, dev_ref_string, dev_hash_table, dev_coordinate, 5, 5, dev_result);
 
-	cudaMemcpy(test_result, dev_result, sizeof(final_result), cudaMemcpyDeviceToHost );
+	cudaMemcpy(test_result, dev_result, sizeof(final_result),
+			cudaMemcpyDeviceToHost);
 	cout << "Fragment     : " << (*test_result).fragment << endl;
 	cout << "Spilled      : " << (*test_result).spilled << endl;
 	cout << "Result Size  : " << (*test_result).size << endl;
-	for(int i = 0; i < (*test_result).size; i++) {
-		cout << "Result coor  : " << (*test_result).coor_results[i].coordiante << endl;
-		cout << "Result diff  : " << (*test_result).coor_results[i].diff_num << endl;
+	for (int i = 0; i < (*test_result).size; i++) {
+		cout << "Result coor  : " << (*test_result).coor_results[i].coordiante
+				<< endl;
+		cout << "Result diff  : " << (*test_result).coor_results[i].diff_num
+				<< endl;
 	}
 
 	free(test_result);
-	cudaFree( dev_fragment );
-	cudaFree( dev_result );
+	cudaFree(dev_fragment);
+	cudaFree(dev_result);
 	freeHash();
 	freeRef();
 	return 0;
