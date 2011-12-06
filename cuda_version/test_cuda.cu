d164d7268921265fcb231cdff0dd684e9fced726
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-12-06 17:24:59 +0000

    Hongyi Helped Donghyuk to commit a compile GPU version

--- cuda_version/test_cuda.cu
+++ cuda_version/test_cuda.cu
@@ -5,6 +5,10 @@
  *	  Author: mac
  */
 #include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <stdio.h>
+
 #include "universal_header.h"
 
 using namespace std;
@@ -12,7 +16,8 @@ using namespace std;
 int test_cuda(void) {
 	long long coord_num = loadHash("toy_hash_0");
 	GPU_fragment test_fragment;
-	test_fragment.fragment = "GGGTGGTAGGTGCAGAGACGGGAGGGGCAGAGCCGCAGGCACAGCCAAGAGGGCTGAAGAAATGGTAGAACGGAGCAGCTGGTGATGTGTGGGCCCACCGGCCCCAGGCTCCTGTCTCCC";
+	//test_fragment.fragment = "GGGTGGTAGGTGCAGAGACGGGAGGGGCAGAGCCGCAGGCACAGCCAAGAGGGCTGAAGAAATGGTAGAACGGAGCAGCTGGTGATGTGTGGGCCCACCGGCCCCAGGCTCCTGTCTCCC";
+	strcpy(test_fragment.fragment, "GGGTGGTAGGTGCAGAGACGGGAGGGGCAGAGCCGCAGGCACAGCCAAGAGGGCTGAAGAAATGGTAGAACGGAGCAGCTGGTGATGTGTGGGCCCACCGGCCCCAGGCTCCTGTCTCCC");
 
 	GPU_fragment* dev_fragment;
 	final_result* dev_result;
@@ -20,13 +25,20 @@ int test_cuda(void) {
 
 	cudaMalloc((void**)&dev_fragment, sizeof(GPU_fragment));
 	cudaMalloc((void**)&dev_result,   sizeof(final_result));
-	cudaMemcpy(dev_fragment, test_fragment, sizeof(GPU_fragment), cudaMemcpyHostToDevice );
+	cudaMemcpy(dev_fragment, &test_fragment, sizeof(GPU_fragment), cudaMemcpyHostToDevice );
 
 	searchFragment <<<1, 1>>> (dev_fragment, 1, dev_ref_string, dev_hash_table, dev_coordinate, 5, 5, dev_result);
 
 	cudaMemcpy(test_result, dev_result, INDEX_NUM*sizeof(final_result), cudaMemcpyDeviceToHost );
+	cout << "Fragment     : " << (*test_result).fragment << endl;
+	cout << "Spilled      : " << (*test_result).spilled << endl;
+	cout << "Result Size  : " << (*test_result).size << endl;
+	for(int i = 0; i < (*test_result).size; i++) {
+		cout << "Result coor  : " << (*test_result).coor_results[i].coordiante << endl;
+		cout << "Result diff  : " << (*test_result).coor_results[i].diff_num << endl;
+	}
 
-	free(str_coordinate);
+	free(test_result);
 	cudaFree( dev_fragment );
 	cudaFree( dev_result );
 	freeHash();
