ae5af0c49694679849770a7ce039c2af119365ea
blepus <blepus@c01c73e4-119b-c190-b91f-0dae92411588>
2011-12-10 02:30:04 +0000

    DHL: fix initializePath function call

--- cuda_version/test_cuda.cu
+++ cuda_version/test_cuda.cu
@@ -15,8 +15,8 @@ using namespace std;
 
 int test_cuda(void) {
 
-    int ref_num = loadRef("result_ref_0");
-	long long coord_num = loadHash("result_hash_0");
+    int ref_num = loadRef("toy_ref_0");
+	long long coord_num = loadHash("toy_hash_0");
 
 //	hash_test(coord_num);
 //	ref_test(ref_num);
