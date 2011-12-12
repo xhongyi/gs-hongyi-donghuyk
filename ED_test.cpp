480a121800ca8ef87d3c739199bd811135cd60fa
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2011-12-12 19:21:35 +0000

    potential bug in edit_distance.cpp

--- ED_test.cpp
+++ ED_test.cpp
@@ -7,18 +7,20 @@
 
 #include "edit_distance.h"
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-	char test_read[READ_LENGTH];
-	char ref_read[READ_LENGTH];
+	char test_read[READ_LENGTH + 1];
+	char ref_read[READ_LENGTH + 1];
 	set_max_indel_num(5);
 	set_max_diff_num(5);
 	allocatePath();
 	cout << "started" << endl;
-	strcpy(test_read, "ACCTGGTACAAAAGGTCAGTTGATGTTGGCTGCTACTTTTTTGTTGTTTTGAGACAGGGTCTCACTCTGTCACCCAGGCTGGAGTGCAGAGGCCTGATCATGACTCAC");
-	strcpy(ref_read,  "GCACCTGGTAAAAGGTCAGTTGATGTTAGCTGCTACTTTTTTGTTGTTTTGAGACAGGGTCTCACTCTGTCACCCAGGCTGGAGTGCAGAGGCCTGATCATGACTCAC");
+	strcpy(test_read, "CACGTTTTATTTTATTTTTTGAGATGGAGTCTCACTCTGTCACCCAGGCTGGAGTGTGGTGGCGCAATCTTTGCTCACTGCAACCTCCGCCTCCCGGGTTCAAGCGAT" );
+	strcpy(ref_read,  "TTACTATATATATATTTTTTGAGATAGGGTCTCACTCTGTCACCCAGGCTGGAGTGCCCTGACATGATCTTGGCTCACTGCAACCTCCACCTCCCGGGTTCAAGCGAT" );
 	ED_result result = editDistanceCal(test_read, ref_read, 1);
 	if (result.correct) {
 		cout << "Correct, passed the ED test" << endl;
