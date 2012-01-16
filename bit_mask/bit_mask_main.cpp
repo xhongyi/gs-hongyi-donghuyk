f7ddea24a7285f47fc0f1efe24f6fed92d43e30b
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-16 02:02:22 +0000

    added bit_mask_generator

--- bit_mask/bit_mask_main.cpp
+++ bit_mask/bit_mask_main.cpp
@@ -8,9 +8,12 @@ using namespace std;
 int main () {
 	string hash_file_name;
 	string mask_file_name;
+	char temp[50];
 	for (int i = 0; i <= 21; i++) {
-		hash_file_name = sscanf("result_hash_%d", i);
-		mask_file_name = sscanf("result_mask_%d", i);
+		sprintf(temp, "result_hash_%d", i);
+		hash_file_name = temp;
+		sprintf(temp, "result_mask_%d", i);
+		mask_file_name = temp;
 		clear_bit_mask();
 		generate_bit_mask(hash_file_name);
 		write_bit_mask(mask_file_name);
