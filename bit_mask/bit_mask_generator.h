64dcdd72acaa9a29b77f7c6489651d6d6099ce31
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-16 01:52:43 +0000

    added bit_mask_generator

---
+++ bit_mask/bit_mask_generator.h
@@ -0,0 +1,34 @@
+/*
+ * bit_map_generator.h
+ *
+ *  Created on: Jan 15, 2012
+ *      Author: mac
+ */
+
+#ifndef BIT_MAP_GENERATOR_H_
+#define BIT_MAP_GENERATOR_H_
+
+#include "common.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int bit_mask[INDEX_NUM];
+
+/*
+ * Fill zeros in to the array
+ */
+void clear_bit_mask();
+
+/*
+ * Read in a hash_table file named hash_table_name
+ */
+void generate_bit_mask(string hash_table_name);
+
+/*
+ * Write bit mask to a file so later it can be read in as well. The file name would be bit_mask_name
+ */
+void bit_mask_writer(string bit_mask_name);
+#endif /* BIT_MAP_GENERATOR_H_ */
---
+++ bit_mask/bit_mask_main.cpp
@@ -0,0 +1,18 @@
+#include "bit_mask_generator.h"
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main () {
+	string hash_file_name;
+	string mask_file_name;
+	for (int i = 0; i <= 21; i++) {
+		hash_file_name = sscanf("result_hash_%i", i);
+		mask_file_name = sscanf("result_mask_%i", i);
+		clear_bit_mask();
+		generate_bit_mask(hash_file_name);
+		write_bit_mask(mask_file_name);
+	}
+}
