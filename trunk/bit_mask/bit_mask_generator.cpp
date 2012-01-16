c7f4680dc5783d61bb4d7637b059a3dda8e59580
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-16 01:52:43 +0000

    added bit_mask_generator

---
+++ trunk/bit_mask/bit_mask_generator.cpp
@@ -0,0 +1,48 @@
+/*
+ * bit_mask_generator.cpp
+ *
+ *  Created on: Jan 15, 2012
+ *      Author: mac
+ */
+int mask_range;
+
+#include "bit_mask_generator.h"
+
+void clear_bit_mask() {
+	for (int i = 0; i < INDEX_NUM; i++)
+		bit_mask[i] = 0;
+}
+
+void generate_bit_mask(string hash_table_name) {
+	int total_number;
+	ifstream hash_file;
+	if (!hash_file.open(hash_table_name.c_str() ) ) {
+		cerr << "File " << hash_table_name << " does not exist" << endl;
+	}
+
+	hash_file >> total_number;
+	//This will return the range of the 1 bit in the bit_mask
+	mask_range = total_number / 32 + 1;
+
+	int index;
+	int coordinate;
+
+	// Read index number & coordinate based on index number
+	for (int i = 0; i < INDEX_NUM; i++) {
+		hash_file >> index;
+		// Read coordinate
+		for (int j = 0; j < index; j++) {
+			hash_file >> coordinate;
+			bit_mask[i] |= 1 << (data / mask_range);
+		}
+	}
+	hash_file.close();
+}
+
+void bit_mask_writer(string bit_mask_name) {
+	ofstream mask_file;
+	mask_file.open(bit_mask_name.c_str() );
+	mask_file << mask_range << endl;
+	for (int i = 0; i < INDEX_NUM; i++)
+		make_file << bit_mask[i] << " ";
+}
