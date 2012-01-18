891ec033dd3a4ca9bfd93b9c96d1ea2e9c3f4e7a
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-18 20:55:25 +0000

    bit mask mile stone 2

--- trunk/bit_mask/test_normal.c
+++ trunk/bit_mask/test_normal.c
@@ -12,14 +12,14 @@
 #include "edit_normal.h"
 
 int main(int argc, char* argv[]) {
-	if (argc >= 3) {
+	if (argc >= 4) {
 		char temp = *argv[1];
 		set_max_indel_num(atoi(&temp));
 		set_max_diff_num(atoi(&temp));
 		if (!strcmp(argv[2], "simple")){
 			set_print_detail(false);
 			fprintf(stdout,"Output Mode : simple\n");
-		} 
+		}
 		else if (!strcmp(argv[2], "detail")){
 			set_print_detail(true);
 			fprintf(stdout,"Output Mode : detail\n");
@@ -27,6 +27,14 @@ int main(int argc, char* argv[]) {
 		else {
 			set_print_detail(true);
 			fprintf(stdout,"Output Mode : detail\n");
+		}
+		if (!strcmp(argv[3], "bm_on") ) {
+			set_bit_mask(true);
+			fprintf(stdout, "Bit mask: on\n");
+		}
+		else {
+			set_bit_mask(false);
+			fprintf(stdout, "Bit mask: off\n");
 		}
 		char print_detail = *argv[2];
 		fprintf(stdout,"Max indel & diff : %d\n", atoi(&temp));
@@ -36,32 +44,34 @@ int main(int argc, char* argv[]) {
 		set_max_indel_num(atoi(&temp));
 		set_max_diff_num(atoi(&temp));
 		set_print_detail(true);
+		set_bit_mask(false);
 		fprintf(stdout,"Output Mode : Detail\n");
+		fprintf(stdout, "Bit mask: off\n");
 		fprintf(stdout,"Max indel & diff : %d\n", atoi(&temp));
 	}
 	else {
 		set_max_indel_num(3);
 		set_max_diff_num(3);
 		set_print_detail(true);
+		set_bit_mask(false);
 		fprintf(stdout,"Output Mode : Detail\n");
+		fprintf(stdout, "Bit mask: off\n");
 		fprintf(stdout,"Max indel & diff : 3\n");
 	}
 	set_fragment_length(101);
 	set_key_number(fragment_length_/KEY_LENGTH);
-	char * file_ref	= (char*) malloc(sizeof(char)*50);	
-	char * file_input	= (char*) malloc(sizeof(char)*50);	
-	char * file_hash	= (char*) malloc(sizeof(char)*50);
-	char * file_dist	= (char*) malloc(sizeof(char)*50);
+	char file_ref [50];
+	char file_input [50];
+	char file_hash [50];
+	char file_mask [50];
+	char file_dist [50];
 	sprintf(file_ref,  "%s", "result_ref_");
 	sprintf(file_hash, "%s", "result_hash_");
 	sprintf(file_input, "%s", "result_input");
+	sprintf(file_mask, "%s", "result_mask_");
 	set_select_cheapest(true);
 	fprintf(stdout,"Mode : Alignment\n");
 	sprintf(file_dist, "%s", "result_dist_cheap_");
-	edit_normal(file_hash, file_ref, file_dist, file_input);
-	free(file_ref);
-	free(file_input);
-	free(file_hash);
-	free(file_dist);
+	edit_normal(file_hash, file_mask, file_ref, file_dist, file_input);
 	return 0;
 }
