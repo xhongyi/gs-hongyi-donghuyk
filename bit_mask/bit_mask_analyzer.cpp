62eb31b8a2284ffdfc2267db054111c4522d20fa
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-29 03:01:21 +0000

    better format for bm_analyzer

---
+++ bit_mask/bit_mask_analyzer.cpp
@@ -0,0 +1,67 @@
+/*
+ * bit_mask_analyzer.cpp
+ *
+ *  Created on: Jan 27, 2012
+ *      Author: hxin
+ */
+
+#include "bit_mask_analyzer.h"
+
+int* hash_table;
+int* coordinate;
+int bot_bit_mask[INDEX_NUM];
+int top_bit_mask[INDEX_NUM];
+
+void initialization() {
+	maskReset(top_bit_mask);
+	maskReset(bot_bit_mask);
+}
+
+void unloadBitMask() {
+	maskReset(top_bit_mask);
+	maskReset(bot_bit_mask);
+	free(hash_table);
+	free(coordinate);
+}
+
+void loadBitMask(string hash_table_name, string top_bm_name,
+		string bot_bm_name) {
+	int temp;
+	hashReconstructor(&hash_table, &coordinate, hash_table_name.c_str());
+	cout << "aaa" << endl;
+	maskLoader(top_bit_mask, temp, top_bm_name);
+	cout << "bbb" << endl;
+	maskLoader(bot_bit_mask, temp, bot_bm_name);
+}
+
+void evaluateBitMask(int threshold, ofstream &outfile) {
+	for (int i = 0; i < INDEX_NUM; i++) {
+		evaluateBitMaskSingle(i, threshold, outfile);
+	}
+}
+
+void evaluateBitMaskSingle(int hash_val, int threshold, ofstream& outfile) {
+	//Test coordinate size. If the coordinate size is too small, it is not
+	//interested
+	if (coordinate[hash_table[hash_val]] > threshold) {
+		outfile << "Hash Val: " << hash_val << endl;
+		outfile << "coordinate size: " << coordinate[hash_table[hash_val]]
+				<< endl;
+		outfile << "top_bit_mask: ";
+		for (int i = 31; i >= 0; i--) {
+			if ((1 << i) & top_bit_mask[hash_val])
+				outfile << 1;
+			else
+				outfile << 0;
+		}
+		outfile << endl;
+		outfile << "bot_bit_mask: ";
+		for (int i = 31; i >= 0; i--) {
+			if ((1 << i) & bot_bit_mask[hash_val])
+				outfile << 1;
+			else
+				outfile << 0;
+		}
+		outfile << endl;
+	}
+}
---
+++ bit_mask/bm_analyzer_main.cpp
@@ -0,0 +1,41 @@
+/*
+ * bm_analyzer_main.cpp
+ *
+ *  Created on: Jan 28, 2012
+ *      Author: hxin
+ */
+
+
+#include "bit_mask_analyzer.h"
+#include <string>
+
+using namespace std;
+
+int main () {
+	initialization();
+	for (int i = 0; i < 1/*MAX_CONTIG_FILE*/; i++) {
+		char file_hash[50];
+		char top_bm_file[50];
+		char bot_bm_file[50];
+		char file_store[50];
+
+		sprintf(file_hash, "%s%i", "result_hash_", i);
+		sprintf(top_bm_file, "%s%i", "result_top_mask_", i);
+		sprintf(bot_bm_file, "%s%i", "result_bot_mask_", i);
+		sprintf(file_store, "%s%i", "bm_analyze_", i);
+
+		string hash_table_name = file_hash;
+		string top_bm_name = top_bm_file;
+		string bot_bm_name = bot_bm_file;
+
+		loadBitMask(hash_table_name, top_bm_name, bot_bm_name);
+
+		ofstream outfile;
+		outfile.open(file_store);
+		evaluateBitMask(10000, outfile);
+		outfile.close();
+
+		unloadBitMask();
+	}
+	return 0;
+}
