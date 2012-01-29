2479158f4273caad495646b839cb138b65835fa5
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-01-29 03:01:30 +0000

    better format for bm_analyzer

---
+++ trunk/bit_mask/bit_mask_analyzer.h
@@ -0,0 +1,29 @@
+/*
+ * bit_mask_analyzer.h
+ *
+ *  Created on: Jan 27, 2012
+ *      Author: hxin
+ */
+
+#include "common.h"
+#include <fstream>
+#include <iostream>
+#include "hash_loader.h"
+
+using namespace std;
+
+#ifndef BIT_MASK_ANALYZER_H_
+#define BIT_MASK_ANALYZER_H_
+
+void initialization();
+
+void unloadBitMask();
+
+void loadBitMask(string hash_table_name, string top_bm_name,
+		string bot_bm_name);
+
+void evaluateBitMask(int threshold, ofstream& outfile);
+
+void evaluateBitMaskSingle(int hash_val, int threshold, ofstream& outfile);
+
+#endif /* BIT_MASK_ANALYZER_H_ */
