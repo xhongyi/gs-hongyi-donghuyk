dabe778c2e4819401632d28d63cca6a8dc2becd9
gohongyi <gohongyi@c01c73e4-119b-c190-b91f-0dae92411588>
2012-03-01 21:22:09 +0000

    friend_hash finished

--- trunk/bit_mask/friend_hash_generator_main.cpp
+++ trunk/bit_mask/friend_hash_generator_main.cpp
@@ -4,22 +4,25 @@
  *  Created on: Mar 1, 2012
  *      Author: hxin
  */
-#include "common.h"
 #include "friend_hash_generator.h"
 #include <cstdio>
 #include <cstdlib>
+#include "common.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
 	initializeFriendHash();
-	for (int i = 0; i < MAX_CONTIG_FILE; i++) {
+	for (int i = 0; i < 1/*MAX_CONTIG_FILE*/; i++) {
 		char ref_name[50];
 		char friend_hash_name[50];
 
 		sprintf(ref_name, "%s%i", "result_ref_", i);
 		sprintf(friend_hash_name, "%s%i", "result_friend_", i);
+		cout << "start analyzing" << endl;
 		analyzeContig(ref_name);
+		cout << "start writing files" << endl;
 		writeFriendHash(friend_hash_name);
 	}
 	return 0;
